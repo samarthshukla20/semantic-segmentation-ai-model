8a05607128d3c011832cddc7ceba48f69cf7dae1
samarthshukla20 <samarthshukla206@gmail.com>
2026-07-24 22:47:44 +0530

    minor changes made

--- web-ui/src/components/Footer.jsx
+++ web-ui/src/components/Footer.jsx
@@ -7,10 +7,10 @@ export default function Footer() {
                 <div className="footer__left">
                     <span className="footer__brand">desertnav</span>
                     <span className="footer__sep">·</span>
-                    <span className="footer__meta">Startathon 2026</span>
+                    <span className="footer__meta">Startathon 2025</span>
                 </div>
                 <div className="footer__right">
-                    <span className="footer__names">Dhruv Bajpai, Samarth Shukla, Kshitij Trivedi</span>
+                    <span className="footer__names">Made by - Samarth Shukla</span>
                 </div>
             </div>
         </footer>
